019c5faadc232046c61bb2be6ad7f685b8349000
wilburforce83 <wilburforce83@gmail.com>
2024-05-17 19:19:29 +0100

    typos

--- services/services.js
+++ services/services.js
@@ -5,13 +5,15 @@ const JSONdb = require("simple-json-db");
 // Example data (could be replaced with a database)
 const db = new JSONdb("./db/db.json");
 const dbhelper = require("../db/helpers");
-const { fetchCMEData } = require("./cmeFetcher");
+const { fetchCMEData } = require("./cmeData");
 
 function services () {
   //trigger your data aquisition events here and save them to the /db/db.json file with simple-json-db
   // Call fetchCMEData initially and then every 5 minutes
   fetchCMEData();
   setInterval(fetchCMEData, 300000);
+
+  console.log('Data services started successfully')
 };
 
 module.exports = { services };
